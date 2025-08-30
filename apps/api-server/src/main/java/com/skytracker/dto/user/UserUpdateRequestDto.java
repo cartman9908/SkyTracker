c48fe7006880e65dd0a53f7b3d9b8174a6712ec8
kanghanseo <kanghanseo2015@gmail.com>
2025-08-30 19:20:21 +0900

    CustomValidation 작성 완료

--- apps/api-server/src/main/java/com/skytracker/dto/user/UserUpdateRequestDto.java
+++ apps/api-server/src/main/java/com/skytracker/dto/user/UserUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package com.skytracker.dto.user;
 
+import com.skytracker.validation.ValidationGroups;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserUpdateRequestDto {
 
+    @NotBlank(message = "이메일을 입력해주세요.", groups = ValidationGroups.NotBlankGroups.class)
     private String email;
 }
