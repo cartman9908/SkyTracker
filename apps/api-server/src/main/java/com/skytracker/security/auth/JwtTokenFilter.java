9c4ecc3418f63c11dfb0d8ec2ee10bfddc2975e1
YeonWoo <kobin4215@naver.com>
2025-11-08 17:54:37 +0900

    feat: jwt token인증 오류 수정중

--- apps/api-server/src/main/java/com/skytracker/security/auth/JwtTokenFilter.java
+++ apps/api-server/src/main/java/com/skytracker/security/auth/JwtTokenFilter.java
@@ -11,12 +11,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @Slf4j
 @Component
@@ -28,45 +25,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
 
     @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        String[] excludePatterns = {
-                "/oauth2/**",
-                "/login/oauth2/**",
-                "/api/flights/search",
-                "/api/flights/hot-routes",
-                "/api/user/refresh-token"
-        };
-
-        String path = request.getRequestURI();
-        AntPathMatcher matcher = new AntPathMatcher();
-        return Arrays.stream(excludePatterns).anyMatch(p -> matcher.match(p, path));
-    }
-
-    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        String method = request.getMethod();
-        String uri = request.getRequestURI();
-        String query = request.getQueryString();
-        String referer = request.getHeader("Referer");
-        String ua = request.getHeader("User-Agent");
-
-        String header = request.getHeader("Authorization");
-
-        log.info("Req {} {}{} | Referer={} | UA={} | Authorization={}",
-                method, uri, (query != null ? "?" + query : ""), referer, ua, header);
-
-        log.info("Authorization Header = {}", header);
-
-        if(header == null || !header.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-
-        String token = header.substring(7);
-
-        log.info("token = {}", token);
+        String token = resolveToken(request);
 
         if (token == null) {
             filterChain.doFilter(request, response);
@@ -88,19 +48,22 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         }
 
         if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            // 이미 인증된 상태면 JWT 인증 스킵
             filterChain.doFilter(request, response);
             return;
         }
 
         CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(username);
-
         if (jwtUtils.isValidToken(token, userDetails.getUsername())){
             log.info("Successfully validate token");
             setAuthentication(userDetails, request);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        return (bearerToken != null && bearerToken.startsWith("Bearer ")) ? bearerToken.substring(7) : null;
     }
 
     private void setAuthentication(CustomUserDetails customUserDetails, HttpServletRequest request) {
