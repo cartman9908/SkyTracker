a01ecb110384bd5b81ec911cd38b3821918075a7
kanghanseo <kanghanseo2015@gmail.com>
2025-11-11 14:41:09 +0900

    kakao OAuth2 로그인 구현 수정

--- apps/api-server/src/main/java/com/skytracker/security/oauth2/KakaoUserInfo.java
+++ apps/api-server/src/main/java/com/skytracker/security/oauth2/KakaoUserInfo.java
@@ -1,14 +1,16 @@
 package com.skytracker.security.oauth2;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
 public class KakaoUserInfo implements Oauth2UserInfo{
 
     private final Map<String, Object> attributes;
-
 
     @Override
     public String getProviderId() {
@@ -22,11 +24,25 @@ public class KakaoUserInfo implements Oauth2UserInfo{
 
     @Override
     public String getEmail() {
-        return (String) attributes.get("email");
+        Map<String, Object> kakaoAccount = getKakaoAccount(attributes);
+
+        return (String) kakaoAccount.get("email");
     }
 
     @Override
     public String getName() {
-        return (String) attributes.get("name");
+        Map<String, Object> kakaoAccount = getKakaoAccount(attributes);
+
+        return (String) kakaoAccount.get("name");
+    }
+
+    private Map<String, Object> getKakaoAccount(Map<String, Object> attributes) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<Map<String, Object>> typeRef = new TypeReference<>() {
+        };
+
+        Object kakaoAccount = attributes.get("kakao_account");
+
+        return objectMapper.convertValue(kakaoAccount, typeRef);
     }
 }
