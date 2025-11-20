bd8fd653a74055c2d22c31c158fcc77c5b85423d
kanghanseo <kanghanseo2015@gmail.com>
2025-11-20 14:57:33 +0900

    401에러 추가

--- apps/api-server/src/main/java/com/skytracker/controller/GlobalExceptionHandler.java
+++ apps/api-server/src/main/java/com/skytracker/controller/GlobalExceptionHandler.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> buildUncaughtException(Exception e) {
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> buildUncaughtException(Exception e) {
+//        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+//
+//        return ResponseEntity.status(status).body(new ErrorResponse("Unknown error occurred", e.getMessage()));
+//    }
 
-        return ResponseEntity.status(status).body(new ErrorResponse("Unknown error occurred", e.getMessage()));
-    }
-
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> buildErrorResponseException(BusinessException e) {
-
-        ErrorCode errorCode = e.getErrorCode();
-
-        return ResponseEntity.status(errorCode.httpStatus).body(new ErrorResponse(errorCode.code, errorCode.message));
-    }
+//    @ExceptionHandler(BusinessException.class)
+//    public ResponseEntity<Object> buildErrorResponseException(BusinessException e) {
+//
+//        ErrorCode errorCode = e.getErrorCode();
+//
+//        return ResponseEntity.status(errorCode.httpStatus).body(new ErrorResponse(errorCode.code, errorCode.message));
+//    }
 }
