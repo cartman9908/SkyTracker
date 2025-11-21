fe4f2c4e9dfd027bfd4179ee662f065768236b4f
kanghanseo <kanghanseo2015@gmail.com>
2025-11-21 16:15:02 +0900

    알림 로직 수정

--- apps/price-alert/src/main/java/com/skytracker/pricealert/config/PriceAlertConsumerConfig.java
+++ apps/price-alert/src/main/java/com/skytracker/pricealert/config/PriceAlertConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.skytracker.pricealert.config;
 
 import com.skytracker.pricealert.config.handler.ConsumerErrorsHandler;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -50,20 +51,6 @@ public class PriceAlertConsumerConfig {
     }
 
     @Bean
-    public ProducerFactory<String, Object> priceAlertProducerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> priceAlertKafkaTemplate() {
-        return new KafkaTemplate<>(priceAlertProducerFactory());
-    }
-
-    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
@@ -73,17 +60,17 @@ public class PriceAlertConsumerConfig {
         return factory;
     }
 
-        @Bean
-        public RetryTopicConfiguration retryTopicConfig(KafkaTemplate<String, Object> priceAlertKafkaTemplate) {
-            return RetryTopicConfigurationBuilder
-                    .newInstance()
-                    .includeTopic("flight-alert")
-                    .autoCreateTopicsWith(PriceAlertConsumerProperties.REPLICATION_FACTOR, PriceAlertConsumerProperties.REPLICA_COUNT)
-                    .maxAttempts(PriceAlertConsumerProperties.MAX_ATTEMPT_COUNT)
-                    .fixedBackOff(PriceAlertConsumerProperties.BACKOFF_PERIOD)
-                    .listenerFactory(kafkaListenerContainerFactory())
-                    .dltSuffix(".DLT")
-                    .dltHandlerMethod(new EndpointHandlerMethod(ConsumerErrorsHandler.class, "postProcessDltMessage"))
-                    .create(priceAlertKafkaTemplate);
-        }
+//        @Bean
+//        public RetryTopicConfiguration retryTopicConfig(KafkaTemplate<String, Object> priceAlertKafkaTemplate) {
+//            return RetryTopicConfigurationBuilder
+//                    .newInstance()
+//                    .includeTopic("flight-alert")
+//                    .autoCreateTopicsWith(PriceAlertConsumerProperties.REPLICATION_FACTOR, PriceAlertConsumerProperties.REPLICA_COUNT)
+//                    .maxAttempts(PriceAlertConsumerProperties.MAX_ATTEMPT_COUNT)
+//                    .fixedBackOff(PriceAlertConsumerProperties.BACKOFF_PERIOD)
+//                    .listenerFactory(kafkaListenerContainerFactory())
+//                    .dltSuffix(".DLT")
+//                    .dltHandlerMethod(new EndpointHandlerMethod(ConsumerErrorsHandler.class, "postProcessDltMessage"))
+//                    .create(priceAlertKafkaTemplate);
+//        }
 }
