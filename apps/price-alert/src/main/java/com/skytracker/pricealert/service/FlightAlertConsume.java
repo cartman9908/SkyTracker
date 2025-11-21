0fa434e12729995d4f75157d270182dc15aeedbf
kanghanseo <kanghanseo2015@gmail.com>
2025-11-21 15:50:31 +0900

    알림 로직 수정

--- apps/price-alert/src/main/java/com/skytracker/pricealert/service/FlightAlertConsume.java
+++ apps/price-alert/src/main/java/com/skytracker/pricealert/service/FlightAlertConsume.java
@@ -2,10 +2,12 @@ package com.skytracker.pricealert.service;
 
 import com.skytracker.common.dto.alerts.FlightAlertEventMessageDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FlightAlertConsume {
@@ -14,8 +16,14 @@ public class FlightAlertConsume {
 
     @KafkaListener(topics = "flight-alert", groupId = "flight-alert-group")
     public void OnMessage(FlightAlertEventMessageDto eventMessageDto, Acknowledgment ack) {
-        sendMailService.sendMail(eventMessageDto);
-
-        ack.acknowledge();
+        log.info("Received event {}", eventMessageDto);
+        try {
+            sendMailService.sendMail(eventMessageDto);
+            log.info("Mail sent successfully for alert: {}", eventMessageDto);
+            ack.acknowledge();
+        } catch (Exception e) {
+            log.error("Failed to send mail for alert: {}", eventMessageDto, e);
+            throw e;
+        }
     }
 }
