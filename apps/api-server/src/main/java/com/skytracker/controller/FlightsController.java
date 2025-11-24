e700ea0e3957c2e4d71b27123ff6f25f402a9f77
YeonWoo <kobin4215@naver.com>
2025-11-24 19:39:23 +0900

    feat: 항공권 data cache 적용

--- apps/api-server/src/main/java/com/skytracker/controller/FlightsController.java
+++ apps/api-server/src/main/java/com/skytracker/controller/FlightsController.java
@@ -4,6 +4,7 @@ import com.skytracker.common.dto.flightSearch.FlightSearchRequestDto;
 import com.skytracker.common.dto.flightSearch.FlightSearchResponseDto;
 import com.skytracker.core.service.AmadeusFlightSearchService;
 import com.skytracker.core.service.FlightSearchCache;
+import com.skytracker.dto.HotRouteBestPrice;
 import com.skytracker.service.token.AmadeusTokenManger;
 import com.skytracker.service.HotRankingService;
 import com.skytracker.service.SearchLogService;
@@ -62,7 +63,7 @@ public class FlightsController {
 
     @GetMapping("/hot-routes")
     public ResponseEntity<?> getHotRouteBestPrice() {
-        List<FlightSearchResponseDto> result = rankingService.getHotRouteBestPrice();
+        List<HotRouteBestPrice> result = rankingService.getHotRouteBestPrice();
         return ResponseEntity.ok().body(result);
     }
 }
