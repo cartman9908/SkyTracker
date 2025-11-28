d89c0bb946499c0d6dfd36e86423b0eff20a1823
kanghanseo <kanghanseo2015@gmail.com>
2025-11-28 17:17:54 +0900

    시큐리티 formlogin 비활성화

--- apps/api-server/src/main/java/com/skytracker/config/SecurityConfig.java
+++ apps/api-server/src/main/java/com/skytracker/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class  SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers( "/oauth2/**", "/login/oauth2/**",
                                 "/api/flights/search", "/api/flights/hot-routes"
